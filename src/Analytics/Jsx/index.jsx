1fa24a966d996b041d78f4613278542165197f82
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2024-01-22 23:59:02 +0100

    implement most basic structure for dashboard product

--- src/Analytics/Jsx/index.jsx
+++ src/Analytics/Jsx/index.jsx
@@ -1,8 +1,21 @@
-
-
+import { motion } from "framer-motion"
+import "../Styles/index.css"
 
 export default function Analytics(){
-    return <div>
-                <p>This is the analytics page</p>
+    return <div id = "analyticsContainer">
+                    
           </div>
+}
+
+
+function Menu(){
+    return <motion.div>
+
+    </motion.div>
+}
+
+function AnalysisDisplay(){
+    return <motion.div>
+
+    </motion.div>
 }
