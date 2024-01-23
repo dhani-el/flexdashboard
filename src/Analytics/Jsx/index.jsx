2440e7e78664b96b00a173b264d977a300ccef69
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2024-01-23 01:42:08 +0100

    install mui icons, implement first set of data rep

--- src/Analytics/Jsx/index.jsx
+++ src/Analytics/Jsx/index.jsx
@@ -1,12 +1,14 @@
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
+import { AttachMoney,Paid,AccountBalanceWallet, CreditCard,Payments } from "@mui/icons-material";
 import "../Styles/index.css"
+
+
 
 export default function Analytics(){
     return <div id = "analyticsContainer">
-                    
+                 <AnalysisDisplay/>   
           </div>
 }
-
 
 function Menu(){
     return <motion.div>
@@ -16,6 +18,29 @@ function Menu(){
 
 function AnalysisDisplay(){
     return <motion.div>
+                <FirstLevelData/>
+    </motion.div>
+}
+
+function FirstLevelData(){
+    return <motion.div>
+                <SingleFirstLevelData Icon={<Paid/>} label="Fixed Income" amount="170,000" />
+                <SingleFirstLevelData Icon={<CreditCard/>}  label="Fixed Expenses" amount="92,400" />
+                <SingleFirstLevelData Icon={<AttachMoney/>} label="Misc Income" amount="12,100" />
+                <SingleFirstLevelData Icon= {<Payments/>} label="Misc Expenses" amount="92,400" />
+                <SingleFirstLevelData Icon={<AccountBalanceWallet/>} label="Balance" amount="92,400" />
+            </motion.div>
+}
+
+function SingleFirstLevelData({Icon,label,amount}){
+    return <motion.div>
+                <motion.div>
+                    {Icon}
+                </motion.div>
+                <motion.div>
+                    <p>{label}</p>
+                    <p>&#8358; {amount}</p>
+                </motion.div>
 
     </motion.div>
 }
