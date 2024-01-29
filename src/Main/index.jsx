6e52a22ba7dc811690f75951a6abbcc69da7a738
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2024-01-29 01:56:16 +0100

    implement a more sustainalble, implementable and declarative theme mode

--- src/Main/index.jsx
+++ src/Main/index.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import Analytics from "../Analytics/Jsx";
 import Navigation from "../Navigation/jsx";
 import { navConstants } from "../constants";
-
+import "../styles/main.css";
 
 export default function MainContainer(){
     const [page, setPage] = useState(navConstants.HOME)
